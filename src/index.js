4f091609ed745fa5e1de5ac18d1323d4bfa6a985
Serg k <xapgbap1@ya.ru>
2023-11-22 09:12:01 +0300

    all task done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import {createRoot} from 'react-dom/client';
-import App from './app';
-import Store from "./store";
-import {StoreContext} from "./store/context";
+import App from './app.js';
+import Store from './store.js';
 
-const store = new Store();
+const store = new Store({
+  list: [
+    {code: 1, title: 'Название элемента'},
+    {code: 2, title: 'Некий объект'},
+    {code: 3, title: 'Заголовок'},
+    {code: 4, title: 'Очень длинное название элемента из семи слов'},
+    {code: 5, title: 'Запись'},
+    {code: 6, title: 'Шестая запись'},
+    {code: 7, title: 'Седьмая запись'},
+  ],
+});
 
 const root = createRoot(document.getElementById('root'));
 
