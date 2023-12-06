e4db36c86091d89f7d5314dc42a0a92fa510caf5
Serg k <xapgbap1@ya.ru>
2023-12-06 11:23:45 +0300

    pull lecture 3

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {createRoot} from 'react-dom/client';
+import App from './app';
+import Store from "./store";
+import {StoreContext} from "./store/context";
+
+const store = new Store();
+
+const root = createRoot(document.getElementById('root'));
+
+// Первый рендер приложения
+root.render(
+  <StoreContext.Provider value={store}>
+    <App/>
+  </StoreContext.Provider>
+);
